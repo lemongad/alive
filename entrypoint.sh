d1ff813cf6543487ef3d95e94b9bc19a8ba82369
lisa <104255899+lemongad@users.noreply.github.com>
2023-06-01 18:18:51 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 生成配置
+sed -i "s#\S[YONGHU]#S{YONGHU]#g" /main.py
+sed -i "s#[PW]#S[PW]#g" /main.py
+sed -i "s#\S[URL]#S{URL]#g" /main.py
+sed -i "s#\${URL2]#S[URL2]#g" /main.py
+chmod +x /main.py
+cat /main.py
+
+# 循环执行脚本
+a=1
+while true; do
+  echo "第${a}轮保活开始！==="
+  python3 /xx.py
+  echo "第${a}轮保活结束！"
+  let a+=1
+  sleep 10
+done
